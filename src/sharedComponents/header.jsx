095130f2543e4ca4b9c7cc33dfa7d7609f5c597c
mastershif <shifra@amenityanalytics.com>
2021-04-18 23:41:40 +0300

    use AppBar from material ui for header

--- src/sharedComponents/header.jsx
+++ src/sharedComponents/header.jsx
@@ -1,29 +1,18 @@
 import styled from 'styled-components';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import SignInButton from "./SignInButton";
-import SignUpButton from "./SignUpButton";
-import React from "react";
-import theme from "../styles/theme";
-
-const HeaderContainer = styled.div`
-  width: 99vw;
-  margin-top: 0.25em;
-  margin-bottom: 0.25em;
-  padding-left: 1em;
-  padding-right: 1em;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-`;
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import appTheme from "../styles/theme";
+import SearchIcon from "@material-ui/icons/Search";
+import AddIcon from '@material-ui/icons/Add';
 
 const Logo = styled.a`
-  color: ${theme.palette.secondary.main};
+  color: ${appTheme.palette.secondary.main};
   line-height: 1.15;
   font-size: 3rem;
   font-weight: 500;
@@ -33,25 +22,46 @@ const Logo = styled.a`
   margin-right: 0.75em;
 `;
 
-const ProfileIconContainer = styled.a`
-  margin: 4px;
-  font-size: 3rem;
-  text-decoration: none;
-`;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    textButton: {
+        fontSize: 20,
+    },
+    iconButton: {
+        fontSize: 20,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 const Header = () => {
+
+    const classes = useStyles();
+
     return (
-        <HeaderContainer>
-            <Logo href="/">Study With Me</Logo>
-            <ButtonsContainer>
-                <SignUpButton />
-                <SignInButton />
-                <ProfileIconContainer href="/profile">
-                    <PersonOutlineIcon fontSize={'inherit'} />
-                </ProfileIconContainer>
-            </ButtonsContainer>
-        </HeaderContainer>
-    )
+        <div className={classes.root}>
+            <AppBar color={"transparent"} position="static" elevation={0}>
+                <Toolbar>
+                    <div className={classes.title}>
+                        <Logo edge="start" href="/">Study With Me</Logo>
+                    </div>
+                    <Button className={classes.textButton} color="inherit" href="/signIn">התחברות / הרשמה</Button>
+                    <IconButton className={classes.iconButton} aria-label="search" color="inherit" href="/search">
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton className={classes.iconButton} aria-label="create" color="inherit" href="/createGroup">
+                        <AddIcon />
+                    </IconButton>
+                    <IconButton className={classes.iconButton} aria-label="profile" color="inherit" href="/profile">
+                        <PersonOutlineIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
 
 export default Header;
