744921a822a502f903fb74e59d083dd760944ec8
Roee <roy_has@yahoo.com>
2021-05-01 14:04:09 +0300

    fixed margins, profile picture, and save button on profileSettings page

--- src/pages/profileSettings components/profileProperties.jsx
+++ src/pages/profileSettings components/profileProperties.jsx
@@ -4,8 +4,8 @@ import Grid from "@material-ui/core/Grid";
 import { major_minor } from "./Categories data/categoriesDB";
 import { degree } from "./Categories data/categoriesDB";
 import { institutions } from "./Categories data/categoriesDB";
-import IconButton from "@material-ui/core/IconButton";
-import UpdateOutlinedIcon from "@material-ui/icons/UpdateOutlined";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 class ProfileProperties extends Component {
   state = {
@@ -40,154 +40,113 @@ class ProfileProperties extends Component {
   render() {
     return (
       <div>
-        <Grid
-          container
-          spacing={8}
-          direction="row"
-          alignItems="center"
-          justify="center"
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+          class="flex-container"
         >
-          <Grid item xs={12} md={6}>
-            <div style={{ display: "flex" }}>
-              <Grid container spacing={10}>
-                <Grid item xs={12}>
-                  <h3 style={{ padding: "0px 0px 20px 0px" }}>כתובת אימייל:</h3>
-                  <h3 style={{ padding: "18px 0px 20px 0px" }}>שם משתמש:</h3>
-                  <h3 style={{ padding: "18px 0px 20px 0px" }}>
-                    מוסד לימודים:
-                  </h3>
-                </Grid>
+          <div style={{ display: "flex", flexGrow: "6" }}>
+            <Grid container>
+              <Grid item xs={12}>
+                <h3
+                  style={{ padding: "0px 0px 20px 0px", textAlign: "center" }}
+                >
+                  כתובת אימייל:
+                </h3>
+                <h3
+                  style={{ padding: "18px 0px 20px 0px", textAlign: "center" }}
+                >
+                  שם משתמש:
+                </h3>
+                <h3
+                  style={{ padding: "18px 0px 20px 0px", textAlign: "center" }}
+                >
+                  מוסד לימודים:
+                </h3>
               </Grid>
-              <Grid container spacing={0}>
-                <Grid item xs={12}>
-                  <h3>{this.state.emailAddress}</h3>
-                  <div style={{ display: "flex", alignItems: "flex-end" }}>
-                    <ProfileProperty
-                      id="שם משתמש"
-                      userValue={this.state.username}
-                      handleChange={this.handleUsernameChange}
-                    />
-
-                    <IconButton
-                      style={{
-                        cursor: "hover",
-                        backgroundColor: "transparent",
-                        padding: "0px 0px 0px 0px",
-                      }}
-                      onClick={() => console.log(this.state.username)}
-                    >
-                      <UpdateOutlinedIcon
-                        color="primary"
-                        style={{ fontSize: 35, padding: 15 }}
-                      />
-                    </IconButton>
-                  </div>
-                  <div style={{ display: "flex", alignItems: "flex-end" }}>
-                    <ProfileProperty
-                      id="מוסד"
-                      userValue={this.state.institute}
-                      data={institutions}
-                      handleChange={this.handleInstituteChange}
-                    />
-
-                    <IconButton
-                      style={{
-                        cursor: "hover",
-                        backgroundColor: "transparent",
-                        padding: "0px 0px 0px 0px",
-                      }}
-                      onClick={() => console.log(this.state.institute)}
-                    >
-                      <UpdateOutlinedIcon
-                        color="primary"
-                        style={{ fontSize: 35, padding: 15 }}
-                      />
-                    </IconButton>
-                  </div>
-                </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item xs={12}>
+                <h3>{this.state.emailAddress}</h3>
+                <div style={{ display: "flex", alignItems: "flex-end" }}>
+                  <ProfileProperty
+                    id="שם משתמש"
+                    userValue={this.state.username}
+                    handleChange={this.handleUsernameChange}
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems: "flex-end" }}>
+                  <ProfileProperty
+                    id="מוסד"
+                    userValue={this.state.institute}
+                    data={institutions}
+                    handleChange={this.handleInstituteChange}
+                  />
+                </div>
               </Grid>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <div style={{ display: "flex" }}>
-              <Grid container spacing={10}>
-                <Grid item xs={12}>
-                  <h3 style={{ padding: "20px 0px 20px 0px" }}>תואר:</h3>
-                  <h3 style={{ padding: "17px 0px 20px 0px" }}>חוג ראשי:</h3>
-                  <h3 style={{ padding: "17px 0px 20px 0px" }}>חוג משני:</h3>
-                </Grid>
+            </Grid>
+          </div>
+          <div style={{ display: "flex", flexGrow: "6" }}>
+            <Grid container>
+              <Grid item xs={12}>
+                <h3
+                  style={{ padding: "20px 0px 20px 0px", textAlign: "center" }}
+                >
+                  תואר:
+                </h3>
+                <h3
+                  style={{ padding: "17px 0px 20px 0px", textAlign: "center" }}
+                >
+                  חוג ראשי:
+                </h3>
+                <h3
+                  style={{ padding: "17px 0px 20px 0px", textAlign: "center" }}
+                >
+                  חוג משני:
+                </h3>
               </Grid>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <div style={{ display: "flex", alignItems: "flex-end" }}>
-                    <ProfileProperty
-                      id="תואר"
-                      userValue={this.state.degree}
-                      data={degree}
-                      handleChange={this.handleDegreeChange}
-                    />
-                    <IconButton
-                      style={{
-                        cursor: "hover",
-                        backgroundColor: "transparent",
-                        padding: "0px 0px 0px 0px",
-                      }}
-                      onClick={() => console.log(this.state.degree)}
-                    >
-                      <UpdateOutlinedIcon
-                        color="primary"
-                        style={{ fontSize: 35, padding: 15 }}
-                      />
-                    </IconButton>
-                  </div>
-                  <div style={{ display: "flex", alignItems: "flex-end" }}>
-                    <ProfileProperty
-                      id="חוג ראשי"
-                      userValue={this.state.major}
-                      data={major_minor}
-                      handleChange={this.handleMajorChange}
-                    />
-
-                    <IconButton
-                      style={{
-                        cursor: "hover",
-                        backgroundColor: "transparent",
-                        padding: "0px 0px 0px 0px",
-                      }}
-                      onClick={() => console.log(this.state.major)}
-                    >
-                      <UpdateOutlinedIcon
-                        color="primary"
-                        style={{ fontSize: 35, padding: 15 }}
-                      />
-                    </IconButton>
-                  </div>
-                  <div style={{ display: "flex", alignItems: "flex-end" }}>
-                    <ProfileProperty
-                      id="חוג משני"
-                      userValue={this.state.minor}
-                      data={major_minor}
-                      handleChange={this.handleMinorChange}
-                    />
-                    <IconButton
-                      style={{
-                        cursor: "hover",
-                        backgroundColor: "transparent",
-                        padding: "0px 0px 0px 0px",
-                      }}
-                      onClick={() => console.log(this.state.minor)}
-                    >
-                      <UpdateOutlinedIcon
-                        color="primary"
-                        style={{ fontSize: 35, padding: 15 }}
-                      />
-                    </IconButton>
-                  </div>
-                </Grid>
+            </Grid>
+            <Grid container spacing={0}>
+              <Grid item xs={12}>
+                <div style={{ display: "flex", alignItems: "flex-end" }}>
+                  <ProfileProperty
+                    id="תואר"
+                    userValue={this.state.degree}
+                    data={degree}
+                    handleChange={this.handleDegreeChange}
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems: "flex-end" }}>
+                  <ProfileProperty
+                    id="חוג ראשי"
+                    userValue={this.state.major}
+                    data={major_minor}
+                    handleChange={this.handleMajorChange}
+                  />
+                </div>
+                <div style={{ display: "flex", alignItems: "flex-end" }}>
+                  <ProfileProperty
+                    id="חוג משני"
+                    userValue={this.state.minor}
+                    data={major_minor}
+                    handleChange={this.handleMinorChange}
+                  />
+                </div>
               </Grid>
-            </div>
-          </Grid>
-        </Grid>
+            </Grid>
+          </div>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <Button variant="contained" color="secondary">
+            <SaveIcon
+              style={{ padding: "0px 0px 0px 10px", fontWight: "bold" }}
+            ></SaveIcon>
+            שמור
+          </Button>
+        </div>
       </div>
     );
   }
