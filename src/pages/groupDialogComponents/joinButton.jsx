b366fd632f434f9d46624f215a32a1bd7f3084bf
mastershif <shifra@amenityanalytics.com>
2021-05-12 20:02:34 +0300

    call the server when a user joins a group, with error handling

--- src/pages/groupDialogComponents/joinButton.jsx
+++ src/pages/groupDialogComponents/joinButton.jsx
@@ -2,30 +2,46 @@ import {useState} from "react";
 import {Button} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/AddCircleOutline";
 import JoinOrLeaveAlert from "./joinOrLeaveAlert";
+import { getUserFromLocalStorage } from '../../localStorage.service';
+import FailedToJoinAlert from "./failedToJoinAlert";
 
+const JoinButton = ({ groupId }) => {
+    const [openConfirm, setOpenConfirm] = useState(false);
+    const [openFailedToJoin, setOpenFailedToJoin] = useState(false);
+    const user = getUserFromLocalStorage();
 
-const JoinButton = () => {
-
-    const [open, setOpen] = useState(false);
-
-    const handleJoining = () => {
-        setOpen(true);
-    }
+    const handleJoining = async () => {
+        const response = await fetch("http://localhost:5000/joinGroup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: user.email, groupId }),
+        });
+        if (response.status === 500) {
+            setOpenFailedToJoin(true);
+        } else {
+            setOpenConfirm(true);
+        };
+    };
 
     const handleUndoJoining = () => {
-        setOpen(false);
-    }
+        setOpenConfirm(false);
+    };
 
     return (
         <>
             <Button variant={"contained"} color={"primary"}
                     size={"large"} onClick={handleJoining}
                     startIcon={<AddIcon />}>הצטרפות לקבוצה</Button>
-            <JoinOrLeaveAlert open={open} setOpen={setOpen} handleUndo={handleUndoJoining}
+            <JoinOrLeaveAlert open={openConfirm} setOpen={setOpenConfirm} handleUndo={handleUndoJoining}
                               message={"הצטרפת לקבוצה בהצלחה!"}
             />
+            <FailedToJoinAlert open={openFailedToJoin} setOpen={setOpenFailedToJoin} handleUndo={() => {setOpenFailedToJoin(false)}}
+                              message={"לא הצלחנו לצרף אותך לקבוצה"}
+            />
         </>
-    )
+    );
 }
 
 export default JoinButton;
