69146932b60cc1bc85c19c438de08c619d8fdf5b
nivguetta <77365613+nivguetta@users.noreplay.github.com>
2021-05-15 22:48:32 +0300

    part of the search algorithm

--- src/pages/groupDialogComponents/groupProfile.jsx
+++ src/pages/groupDialogComponents/groupProfile.jsx
@@ -9,60 +9,60 @@ import { useState } from "react";
 import GroupDialog from "../groupDialog";
 
 const GroupProfile = (props) => {
-  const { group, isProfile } = props;
-  const classes = Styles.useStyles();
-  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+    const { group, isProfile } = props;
+    const classes = Styles.useStyles();
+    const [dialogIsOpen, setDialogIsOpen] = useState(false);
 
-  const handleOpenDialog = () => {
-    setDialogIsOpen(true);
-  };
-  const handleCloseDialog = () => {
-    setDialogIsOpen(false);
-  };
+    const handleOpenDialog = () => {
+        setDialogIsOpen(true);
+    };
+    const handleCloseDialog = () => {
+        setDialogIsOpen(false);
+    };
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <GroupOccupancyStatus
-            currentGroupSize={group.currentGroupSize}
-            maxGroupSize={group.maxGroupSize}
-          />
-        }
-        title={<Styles.Title>{group.groupTitle}</Styles.Title>}
-        subheader={`פגישה ${group.meetingType}`}
-      />
-      <CardContent>
-        <Styles.CardRow>
-          <TodayOutlinedIcon />
-          <Styles.Text>
-            {format(parseISO(group.date), "EEEE, 'ה-'d 'ב'MMMM yyyy", {locale: he,})}
-          </Styles.Text>
-        </Styles.CardRow>
-        <Styles.CardRow>
-          <AccessTimeOutlinedIcon />
-          <Styles.Text>
-            מ-{format(parseISO(group.startHour), "HH:mm", { locale: he })} עד{" "}
-            {format(parseISO(group.endHour), "HH:mm", { locale: he })}
-          </Styles.Text>
-        </Styles.CardRow>
-      </CardContent>
-      <Button
-        variant={"outlined"}
-        color={"primary"}
-        size={"large"}
-        onClick={handleOpenDialog}
-      >
-        מידע נוסף
-      </Button>
-      <GroupDialog
-        group={group}
-        isProfile={isProfile}
-        onClose={handleCloseDialog}
-        open={dialogIsOpen}
-      />
-    </Card>
-  );
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <GroupOccupancyStatus
+                        currentGroupSize={group.users.length}
+                        maxGroupSize={group.groupSize}
+                    />
+                }
+                title={<Styles.Title>{group.groupTitle}</Styles.Title>}
+                subheader={`פגישה ${group.meetingType}`}
+            />
+            <CardContent>
+                <Styles.CardRow>
+                    <TodayOutlinedIcon />
+                    <Styles.Text>
+                        {format(parseISO(group.date), "EEEE, 'ה-'d 'ב'MMMM yyyy", {locale: he})}
+                    </Styles.Text>
+                </Styles.CardRow>
+                <Styles.CardRow>
+                    <AccessTimeOutlinedIcon />
+                    <Styles.Text>
+                        מ-{format(parseISO(group.startHour), "HH:mm", { locale: he })} עד{" "}
+                        {format(parseISO(group.endHour), "HH:mm", { locale: he })}
+                    </Styles.Text>
+                </Styles.CardRow>
+            </CardContent>
+            <Button
+                variant={"outlined"}
+                color={"primary"}
+                size={"large"}
+                onClick={handleOpenDialog}
+            >
+                מידע נוסף
+            </Button>
+            <GroupDialog
+                group={group}
+                isProfile={isProfile}
+                onClose={handleCloseDialog}
+                open={dialogIsOpen}
+            />
+        </Card>
+    );
 };
 
 export default GroupProfile;
--- src/pages/searchComponents/searchForm.jsx
+++ src/pages/searchComponents/searchForm.jsx
@@ -1,181 +1,175 @@
-import * as Styles from '../../styles/createGroupStyle';
+import {useState} from "react";
 import {Button, ButtonGroup, Checkbox, FormControl, FormControlLabel,
-    FormGroup, FormLabel, Grid, InputLabel, makeStyles,
-    Radio, RadioGroup, Select, TextField} from "@material-ui/core";
+    FormGroup, FormLabel, Grid, Radio, RadioGroup, TextField} from "@material-ui/core";
+import {Autocomplete} from "@material-ui/lab";
+import * as Styles from "../../styles/searchStyle";
 import {KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 import ClockIcon from "@material-ui/icons/AccessTime";
-import {useState} from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import {sortedCitiesNames} from "../../assets/cities";
-import styled from "styled-components";
+import Fuse from "fuse.js";
+import clsx from "clsx";
 
-const Title = styled.h1`
-  margin: 0 0 5px;
-  line-height: 3.5rem;
-  font-size: xx-large;
-  font-weight: bold;
-  padding: 1rem;
-`;
-
-const MiniTitle = styled.div`
-  line-height: 2.5rem;
-  font-size: large;
-  font-weight: bold;
-`;
-
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-    whiteBackground: {
-        backgroundColor: 'white',
-        padding: '1rem',
-        borderRadius: '10px',
-    },
-    noBackground: {
-        padding: '1rem',
-        borderRadius: '10px',
-    },
-}));
 
 const initialSearchParameters = {
-    groupTitle: '',
-    groupSize: null,
+    text: '',
+    groupSize: '',
     date: null,
     startHour: null,
     endHour: null,
-    isVirtual: 'virtual',
-    place: '',
-    link: '',
+    meetingType: 'הכל',
     institution: false,
-    calendar: false,
     city: ''
 }
 
-const SearchForm = ({ setShowResults }) => {
+const SearchForm = (props) => {
+
+    const {allGroups, setResults, setShowResults} = props;
     const [searchParameters, setSearchParameters] = useState(initialSearchParameters);
+    const classes = Styles.useStyles();
 
-    const classes = useStyles();
+    console.log(searchParameters.groupSize);
 
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setSearchParameters({...searchParameters, [name]:value});
+    };
     const handleDateChange = (selectedDate) => {setSearchParameters({...searchParameters, date: selectedDate})}
     const handleStartHour = (selectedHour) => {setSearchParameters({...searchParameters, startHour: selectedHour})}
     const handleEndHour = (selectedHour) => {setSearchParameters({...searchParameters, endHour: selectedHour})}
     const handleCheckbox = (event) => {setSearchParameters({...searchParameters, [event.target.name]: event.target.checked})}
-    const handleCity = (event) => {setSearchParameters({...searchParameters, [event.target.name]: event.target.value})}
+    const handleCity = (event, value) => {
+        setSearchParameters({...searchParameters, city: value === null ? '' : value})
+    }
 
-    const handleChange = event => {
-        const {name, value} = event.target;
-        setSearchParameters({...searchParameters, [name]:value});
-    };
+    const options = {
+        threshold: 0.0,
+        ignoreLocation: true,
+        minMatchCharLength: true,
+        keys: ["groupTitle", "groupDescription"]
+    }
+
+    const searchAlgorithm = (group) => {
+        return (
+            (searchParameters.meetingType === 'הכל' ?
+                true : group.item.meetingType === searchParameters.meetingType) &&
+            (searchParameters.groupSize === ''
+                ? true : group.item.groupSize <= searchParameters.groupSize)
+
+        )
+    }
+
+    let emptyStringSearch = {};
+    if (allGroups) {
+        emptyStringSearch = allGroups.map(group => ({
+            item: Object.assign(group, {}),
+            matches: [],
+            score: 1
+        }))
+    }
 
     const handleSubmit = event => {
         event.preventDefault();
-        console.log('you are searching for a study group');
+        if (allGroups) {
+            const fuse = new Fuse(allGroups, options)
+            let results = (
+                searchParameters.text === "" ? emptyStringSearch : fuse.search(searchParameters.text)
+            )
+            results = results.filter(searchAlgorithm)
+            setResults(results);
+        }
         setShowResults(true);
     };
 
+
+
     return (
         <form onSubmit={handleSubmit}>
-            <Title>חיפוש קבוצה</Title>
+            <Styles.Title>חיפוש קבוצה</Styles.Title>
             <Grid container>
-                <Grid item xs={12} sm={12} className={classes.whiteBackground}>
-                <Grid item xs={12} sm={6}>
-                    <FormGroup style={{margin: "10px 0 0 0"}}>
-                        <Styles.Label style={{margin: "0 0 8px 0"}}>מילות חיפוש</Styles.Label>
-                        <TextField required={false} variant={"outlined"} name={"groupTitle"}
-                                   value={searchParameters.groupTitle} onChange={handleChange}/>
-                    </FormGroup>
-                </Grid>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <FormGroup style={{margin: "10px 0 0 0"}}>
-                            <FormControl component="fieldset" style={{margin: "30px 5px 0"}}>
-                                <FormLabel component="legend">סוג הקבוצה</FormLabel>
-                                <RadioGroup name={"isVirtual"} value={searchParameters.isVirtual}
-                                            row onChange={handleChange}>
-                                    <FormControlLabel value={"virtual"} control={<Radio/>} label={"וירטואלית"} />
-                                    <FormControlLabel value={"frontal"} control={<Radio/>} label={"פרונטלית"} />
-                                </RadioGroup>
-                            </FormControl>
-                        </FormGroup>
+                <Grid container spacing={5} className={clsx(classes.whiteBackground, classes.formControl)}>
+                    <Grid item xs={12} sm={4}>
+                        <FormControl fullWidth>
+                            <FormLabel component="legend">מילות חיפוש</FormLabel>
+                            <TextField variant={"outlined"} name={"text"} type={"search"} margin={"dense"}
+                                       value={searchParameters.text} onChange={handleChange}/>
+                        </FormControl>
                     </Grid>
-                </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend" style={{margin: "0 0 5px"}}>
+                                סוג הקבוצה
+                            </FormLabel>
+                            <RadioGroup name={"meetingType"} value={searchParameters.meetingType}
+                                        row onChange={handleChange}>
+                                <FormControlLabel value={"הכל"} control={<Radio/>} label={"הכל"} />
+                                <FormControlLabel value={"וירטואלית"} control={<Radio/>} label={"וירטואלית"} />
+                                <FormControlLabel value={"פרונטלית"} control={<Radio/>} label={"פרונטלית"} />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
                 </Grid>
                 <Grid item xs={12} sm={12} className={classes.noBackground}>
-                <MiniTitle>סינונים נוספים:</MiniTitle>
-                <Grid container style={{margin: "10px 0 0 0"}}>
-                <Grid item xs={12} sm={12} md={10} lg={6}>
-                    <Grid container>
-                        <Grid item xs={12} sm={4}>
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <KeyboardDatePicker
-                                    required={false} autoOk disablePast disableToolbar
-                                    variant={"inline"} label={"בחר/י תאריך"}
-                                    format={"dd/MM/yyyy"} name={"date"}  value={searchParameters.date}
-                                    onChange={selectedDate => handleDateChange(selectedDate)}/>
-                            </MuiPickersUtilsProvider>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <KeyboardTimePicker
-                                    required={false} autoOk ampm={false} variant={"inline"} label={"טווח שעות - התחלה"}
-                                    minutesStep={5} value={searchParameters.startHour} onChange={handleStartHour}
-                                    keyboardIcon={<ClockIcon/>}/>
-                            </MuiPickersUtilsProvider>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <KeyboardTimePicker
-                                    required={false} autoOk ampm={false} variant={"inline"} label={"טווח שעות - סיום"}
-                                    minutesStep={5} value={searchParameters.endHour} onChange={handleEndHour}
-                                    keyboardIcon={<ClockIcon/>}/>
-                            </MuiPickersUtilsProvider>
+                    <Styles.MiniTitle>סינונים נוספים:</Styles.MiniTitle>
+                    <Grid container style={{margin: "10px 0 0 0"}}>
+                        <Grid item xs={12} sm={12} md={10} lg={6}>
+                            <Grid container>
+                                <Grid item xs={12} sm={4}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            autoOk disablePast disableToolbar
+                                            variant={"inline"} label={"בחר/י תאריך"}
+                                            format={"dd/MM/yyyy"} name={"date"}  value={searchParameters.date}
+                                            onChange={selectedDate => handleDateChange(selectedDate)}/>
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                                <Grid item xs={12} sm={4}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardTimePicker
+                                            autoOk ampm={false} variant={"inline"} label={"טווח שעות - התחלה"}
+                                            minutesStep={5} value={searchParameters.startHour} onChange={handleStartHour}
+                                            keyboardIcon={<ClockIcon/>}/>
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                                <Grid item xs={12} sm={4}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardTimePicker
+                                            autoOk ampm={false} variant={"inline"} label={"טווח שעות - סיום"}
+                                            minutesStep={5} value={searchParameters.endHour} onChange={handleEndHour}
+                                            keyboardIcon={<ClockIcon/>}/>
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
+                    <Grid container>
+                        <Grid item xs={8} sm={4} md={3} lg={2}>
+                            <FormGroup style={{margin: "10px 0 0 0"}}>
+                                <TextField type="number" name="groupSize" label={"מס' משתתפים/ות מקסימלי"}
+                                           value={searchParameters.groupSize} onChange={handleChange}/>
+                            </FormGroup>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <FormGroup style={{margin: "10px 0 0 0"}}>
+                            <FormControlLabel
+                                control={<Checkbox checked={searchParameters.institution}
+                                                   onChange={handleCheckbox} name={"institution"} />}
+                                label={"אני רוצה ללמוד רק עם סטודנטים/יות מהמוסד האקדמי שלי"}/>
+                        </FormGroup>
+                        <Autocomplete options={sortedCitiesNames} value={searchParameters.city}
+                                      name={"city"} onChange={handleCity} fullWidth
+                                      getOptionSelected={(option,value) => value.value === option.value}
+                                      renderInput={(params) =>
+                                          <TextField {...params} label={"עיר"} variant={"standard"} />}
+                        />
+                    </Grid>
                 </Grid>
-                </Grid>
-                <Grid container>
-                <Grid item xs={8} sm={4} md={3} lg={2}>
-                    <FormGroup style={{margin: "10px 0 0 0"}}>
-                        <TextField required={false} type="number" name="groupSize" label={"מס' משתתפים/ות מקסימלי"}
-                                   InputProps={{inputProps: { min: 2, max: 100 }}}
-                                   value={searchParameters.groupSize} onChange={handleChange}/>
-                    </FormGroup>
-                </Grid>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <FormGroup style={{margin: "10px 0 0 0"}}>
-                        <FormControlLabel
-                            control={<Checkbox checked={searchParameters.institution}
-                                               onChange={handleCheckbox} name={"institution"} />}
-                            label={"אני רוצה ללמוד רק עם סטודנטים/יות מהמוסד האקדמי שלי"}/>
-                    </FormGroup>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor="city-native-simple">עיר</InputLabel>
-                        <Select
-                            native
-                            value={searchParameters.city}
-                            onChange={handleCity}
-                            inputProps={{
-                                name: 'city',
-                                id: 'city-native-simple',
-                            }}
-                        >
-                            <option aria-label="None" value="" />
-                            {sortedCitiesNames.map(city => <option value={city}>{city}</option>)}
-                        </Select>
-                    </FormControl>
-                </Grid>
-            </Grid>
-            <ButtonGroup style={{margin: "10px 0 0 0", paddingRight: "1rem"}}>
-                <Button variant={"contained"} color={"primary"} size={"large"} type={"submit"}
-                         startIcon={<SearchIcon />}>חיפוש</Button>
-            </ButtonGroup>
+                <ButtonGroup style={{margin: "10px 0 0 0", paddingRight: "1rem"}}>
+                    <Button variant={"contained"} color={"primary"} size={"large"} type={"submit"}
+                            startIcon={<SearchIcon />}>חיפוש</Button>
+                </ButtonGroup>
             </Grid>
         </form>
     )
