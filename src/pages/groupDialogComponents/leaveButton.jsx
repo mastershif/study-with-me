ce424c8dacddf538a459c743e90c5931044ceada
nivguetta <77365613+nivguetta@users.noreplay.github.com>
2021-05-18 23:33:45 +0300

    fixing the leave button

--- src/pages/groupDialogComponents/leaveButton.jsx
+++ src/pages/groupDialogComponents/leaveButton.jsx
@@ -5,26 +5,44 @@ import JoinOrLeaveAlert from "./joinOrLeaveAlert";
 
 import { getUserFromLocalStorage } from '../../localStorage.service';
 
-const LeaveButton = ({groupId}) => {
+const LeaveButton = (props) => {
 
-    const [open, setOpen] = useState(false);
-    const [error, setError] = useState(null);
+    const {groupId, isProfile} = props;
+    const [open, setOpenConfirm] = useState(false);
     const user = getUserFromLocalStorage();
 
     const handleLeaving = async () => {
-        await fetch("http://localhost:5000/leaveGroup", {
+        const response = await fetch("http://localhost:5000/leaveGroup", {
             method: 'PUT',
             headers: { "Accept": "application/json",
                         "Content-Type": "application/json"},
             body: JSON.stringify({ email: user.email, groupId }),
-        })
-        .then(() => { setOpen(true)}, (error) => {  setOpen(true)
-                                                        setError(error)
-                                                        console.log('Error in fetch operation:' , error)})
+        });
+        if (response.status !== 200) {
+            console.log('not 200');
+            // need to add an alert - if the leaving didn't succeed.
+        }
+        else {
+            console.log('is 200');
+            if (isProfile) {
+                console.log('profile');
+                setTimeout(function () {
+                    window.location.reload();
+                }, 1000)
+            }
+            else {
+                console.log('not profile');
+                setOpenConfirm(true);
+                document.getElementById("searchButton").click();
+                setTimeout(function () {
+                    document.getElementById("searchButton").click();
+                }, 50)
+            }
+        }
     }
 
     const handleUndoLeaving = () => {
-        setOpen(false);
+        setOpenConfirm(false);
     }
 
     return (
@@ -32,7 +50,7 @@ const LeaveButton = ({groupId}) => {
             <Button variant={"contained"} color={"secondary"}
                     size={"large"} onClick={handleLeaving}
                     startIcon={<RemoveIcon />}>צא/י מהקבוצה</Button>
-            <JoinOrLeaveAlert open={open} setOpen={setOpen} handleUndo={handleUndoLeaving}
+            <JoinOrLeaveAlert open={open} setOpen={setOpenConfirm} handleUndo={handleUndoLeaving}
                               message={"יצאת מהקבוצה בהצלחה!"}
             />
         </>
